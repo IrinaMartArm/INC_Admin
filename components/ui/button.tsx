4ade9e3d9da679ca017e6f949863f3da699472cd
Ирина Мартоян <surenira@icloud.com>
2024-09-20 12:00:05 +0400

    setting

---
+++ components/ui/button.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+import { Slot } from '@radix-ui/react-slot'
+import { type VariantProps, cva } from 'class-variance-authority'
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  {
+    defaultVariants: {
+      size: 'default',
+      variant: 'default',
+    },
+    variants: {
+      size: {
+        default: '',
+        icon: '',
+        lg: '',
+        sm: '',
+      },
+      variant: {
+        default: '',
+        link: '',
+        outline: '',
+        secondary: '',
+      },
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ asChild = false, className, size, variant, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+
+    return (
+      <Comp className={cn(buttonVariants({ className, size, variant }))} ref={ref} {...props} />
+    )
+  }
+)
+
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
 
+import { Roboto } from '@next/font/google'
+
+import '../styles/globals.css'
 import '@/styles/index.scss'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={roboto.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
